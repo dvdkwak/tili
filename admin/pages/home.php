9ba92b1c85e4d5cf18d9bab5e40daf215264f182
PieterLoosman <ploosman123@gmail.com>
2018-01-17 11:05:23 +0100

    test
    ja

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -7,7 +7,7 @@
     }
 ?>
 
-<h1>hier komt de homepage van de backend van TiliT</h1>
+<h1>hier komt de homepage van de backend van TiliT oh gelukkig</h1>
 
 <form method="post">
     <input type="hidden" name="logout" value="true">
