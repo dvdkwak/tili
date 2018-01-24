b9660131823515776d50f5a84f544fc0a53b1018
PieterLoosman <ploosman123@gmail.com>
2018-01-24 15:05:41 +0100

    home navbar
    navbar voor de homepage toegevoegd en pacman sound verbeterd

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -1,9 +1,20 @@
-<h1>hier komt de homepage van de backend van TiliT oh gelukkig</h1>
+<!--<h1>hier komt de homepage van de backend van TiliT oh gelukkig</h1>
 
 <form method="post">
     <input type="hidden" name="logout" value="true">
     <input type="submit" value="logout">
-</form>
+</form>-->
+
+<ul class="main-navbar">
+	<li class="main-navbar-item"><a href="news.asp">News</a></li>
+	<li class="main-navbar-item"><a href="contact.asp">Contact</a></li>
+	<li class="main-navbar-item"><a href="about.asp">About</a></li>
+	<li class="main-navbar-item"><a href="default.asp">Home</a></li>
+</ul>
+
+<div class="main-container">
+<p>ja</p>
+</div>
 
 <?php
     if(!isset($_SESSION['status'])){
