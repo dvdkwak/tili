bc624135e79699e7784d92408a0c003164ce8fd5
dvdkwak <30349512+dvdkwak@users.noreply.github.com>
2018-01-29 14:18:00 +0100

    requests changed

--- admin/pages/requests.php
+++ admin/pages/requests.php
@@ -1,18 +1,31 @@
 <div class="main-container">
 	
-	<?php echo $requests->getRequests(); ?>
+	<?php
 
-	<form method="post">
-	    <input type="hidden" name="logout" value="true">
-	    <input type="submit" value="logout">
-	</form>
-</div>
+    $requests = new requests();
+    $data = $requests->getRequests();
 
-
-<?php
-    if(!isset($_SESSION['status'])){
-        echo 'bestaat niet';
-    }else{
-        echo $_SESSION['status'];
+    foreach($data AS $item){
+        echo '
+        <div class="container-fluid">
+        <div id="medewerker" class="container">
+            <div class="card">
+                <div class="card-header custom-header">
+                    <h5 style="color:white;">' . $item['projectName'] . '</hp>
+                        <button type="button" class="snikker btn btn-dark btn-custom-trans float-right"><i class="material-icons">&#xE872;</i></button>
+                        <button type="button" class="snikker btn btn-dark btn-custom-trans float-right"><i class="material-icons">&#xE876;</i></button>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Voetbalclub</h5>
+                    <p class="card-text">' . $item['description'] . '</p>
+                    <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal">Bekijk PvE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+        ';
     }
-?>
+
+    ?>
+
+</div>
