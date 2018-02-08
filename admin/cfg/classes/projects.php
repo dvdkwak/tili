328c663269728a52d44f9e55fadee4748dde09e9
PieterLoosman <ploosman123@gmail.com>
2018-02-08 11:12:03 +0100

    error handling
    huh

--- admin/cfg/classes/projects.php
+++ admin/cfg/classes/projects.php
@@ -13,7 +13,7 @@
 				if ($_SESSION['userlevel'] == 0) {
 					//When userlevel is 0 (Beheerder) show all the projects
 					$query = 'SELECT * FROM tbl_projects WHERE isRequest = "1"';
-				} else if ($_SESSION['userlevel'] == 1) {
+				} else {
 					//When userlevel is 1 (Medewerker) show all the projects where the user is assigned to
 					$query = 'SELECT a.*, b.FK_projects_id, b.FK_users_id FROM tbl_projects AS a
 					INNER JOIN tbl_users_projects AS b ON a.id = b.FK_projects_id WHERE b.FK_users_id = '.$userId;
